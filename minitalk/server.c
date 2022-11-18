607e937170b771124fab64dd670d99c201ee49e3
Martina Magma gerena <mmagma-g@student.42malaga.com>
2022-11-18 18:56:35 +0100

    just trying

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,34 +6,37 @@
 /*   By: mmagma-g <mmagma-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 18:07:50 by mmagma-g          #+#    #+#             */
-/*   Updated: 2022/11/17 19:37:48 by mmagma-g         ###   ########.fr       */
+/*   Updated: 2022/11/18 18:56:05 by mmagma-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	ft_bin_to_char(int sig)
+void	ft_message_received(int sig)
 {
-	int				power;
-	unsigned char	x;
-	size_t			i;
+	static char	str;
+	static int	i;
 
-	power = 2;
-	x = 0;
-	i = ft_strlen(str) + 1;
-	while (i + 1 != 0)
+	if (!i)
 	{
-		x += power * (str[i] - '0');
-		power *= 2;
-		i--;
+		i = 1;
+		str = 0;
 	}
-	write(1, &x, 1);
+	str |= (sig == SIGSR1);
+	if (i++ == 8)
+	{
+		ft_putchar_fd(str, 1);
+		i = 1;
+		str = 0;
+	}
+	else
+		c <<= 1;
 }
 
 int	main(void)
 {
-	signal(SIGUSR1, ft_bin_to_char);
-	signal(SIGUSR2, ft_bin_to_char);
+	signal(SIGUSR1, ft_message_received);
+	signal(SIGUSR2, ft_message_received);
 	ft_printf("The Server PID is: %d\n", getpid());
 	while (1)
 		pause();
